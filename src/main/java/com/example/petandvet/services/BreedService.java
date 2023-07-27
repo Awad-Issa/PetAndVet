c0b3dc8cd393b914f2fdbc976f493fefe91f94d6
p1gt <ranaranap59@gmail.com>
2023-07-27 10:08:36 +0300

    breed service and repo

---
+++ src/main/java/com/example/petandvet/services/BreedService.java
@@ -0,0 +1,39 @@
+package com.example.petandvet.services;
+
+import com.example.petandvet.models.Breed;
+import com.example.petandvet.models.Product;
+import com.example.petandvet.repositories.BreedRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BreedService {
+    private final BreedRepository breedRepo;
+
+    public List<Breed> getAllBreeds() {
+        return (List<Breed>) breedRepo.findAll();
+    }
+
+    public Breed getBreedById(Long id) {
+        return breedRepo.findById(id).orElse(null);
+    }
+
+    public Breed createBreed(Breed breed) {
+        return breedRepo.save(breed);
+    }
+
+    public Breed updateBreed(Breed breed) {
+        return breedRepo.save(breed);
+    }
+
+    public void deleteBreed(Long id) {
+        breedRepo.deleteById(id);
+    }
+
+    public List<Product> getProductsByBreed(Breed breed) {
+        return breed.getProducts();
+    }
+}
