5ae4f6e8aa57270cacfa818fa133cd797f17292f
Hakeem Barghouthi <124943880+Hakeem229@users.noreply.github.com>
2023-07-31 00:52:46 +0300

    HomeController

--- src/main/java/com/example/petandvet/controllers/HomeController.java
+++ src/main/java/com/example/petandvet/controllers/HomeController.java
@@ -37,7 +37,7 @@ public class HomeController {
 
   @PostMapping("/register")
   public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result,
-      Model model, HttpSession session) {
+                         Model model, HttpSession session) {
     User regUser = userServ.register(newUser, result);
     if (result.hasErrors()) {
       model.addAttribute("newLogin", new LoginUser());
@@ -49,7 +49,7 @@ public class HomeController {
 
   @PostMapping("/login")
   public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, BindingResult result,
-      Model model, HttpSession session) {
+                      Model model, HttpSession session) {
     User logUser = userServ.login(newLogin, result);
     if (result.hasErrors()) {
       model.addAttribute("newUser", new User());
